b708311f7337f33838608350da24edb94be4dfbc
cocoa2135 <cocoa2135@gmail.com>
2019-08-12 21:45:15 +0800

    增加依赖本地组件，修改main.js引入my-vue-library，修改App.vue使用依赖的组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HelloWorld from 'my-vue-library'
 
 Vue.config.productionTip = false
 
+Vue.use(HelloWorld)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
