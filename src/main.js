a3fb57575fb4e98937b4aedeffc1d48c7d6d6ea5
cocoa2135 <cocoa2135@gmail.com>
2019-08-13 21:17:35 +0800

    增加按需加载的例子

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import HelloWorld from 'my-vue-library'
+// import xcom from 'xcom-components'
+import {HelloWorld} from 'xcom-components'
+import 'xcom-components/dist/xcom-components.css'
 
 Vue.config.productionTip = false
+// Vue.use(xcom)
+Vue.component('HelloWorld',HelloWorld)
 
-Vue.use(HelloWorld)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
