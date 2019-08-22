5beac3094bda898f8de2c1b33ee3de3aa7840931
cocoa2135 <cocoa2135@gmail.com>
2019-08-22 21:12:03 +0800

    增加格式化配置

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import xcom from 'xcom-components'
-import {HelloWorld} from 'xcom-components'
+import { HelloWorld } from 'xcom-components'
 import 'xcom-components/dist/xcom-components.css'
 
 Vue.config.productionTip = false
 // Vue.use(xcom)
-Vue.component('HelloWorld',HelloWorld)
+Vue.component('HelloWorld', HelloWorld)
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
